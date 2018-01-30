e2fbf87ece4fb87beb6d8339401425eb4ba7d5c9
karthikkct77 <karthikkct77@gmail.com>
2018-01-30 15:42:50 +0530

    User Controller and views

---
+++ application/controllers/User_Controller.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_Controller extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->username == "") {
+            redirect('Welcome');
+        }
+        //$this->load->model('LoginModel','LoginModel');
+        $this->load->helper('url');
+        /***** LOADING HELPER TO AVOID PHP ERROR ****/
+        $this->load->model('Technical_User_Model', 'technical_user_model'); /* LOADING MODEL * Technical_User_Model as technical_user_model */
+        $this->load->library('session');
+    }
+
+    //** Admin Dashboard**//
+    public function dashboard()
+    {
+        $this->load->view('Admin/header');
+        $this->load->view('Admin/left');
+        $this->load->view('Admin/top');
+        $this->load->view('Admin/dashboard');
+        $this->load->view('Admin/footer');
+    }
+
+}
+?>
