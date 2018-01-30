fa382c137ac4211b029b7b08924cd3c9701e1dbf
karthikkct77 <karthikkct77@gmail.com>
2018-01-30 14:50:21 +0530

    ibt karthik

--- application/views/Admin/dashboard.php
+++ application/views/Admin/dashboard.php
@@ -3,7 +3,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        IBT
+        IBT Karthik
         <small>Version</small>
       </h1>
       <ol class="breadcrumb">
