a785579faaedfe9bf87726a012a4fb7c52ab457b
karthikkct77 <karthikkct77@gmail.com>
2018-01-30 12:49:44 +0530

    Dashboard Changed

--- application/views/Admin/dashboard.php
+++ application/views/Admin/dashboard.php
@@ -3,8 +3,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
-        <small>Version 2.0</small>
+        Dashboard Modifications
+        <small>Version 111111</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
