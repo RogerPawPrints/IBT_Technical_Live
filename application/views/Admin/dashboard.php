debcd21bb13c69ee556130574f098d68ce41b4d7
karthikkct77 <karthikkct77@gmail.com>
2018-01-30 15:21:59 +0530

    karthiibt

--- application/views/Admin/dashboard.php
+++ application/views/Admin/dashboard.php
@@ -4,7 +4,7 @@
     <section class="content-header">
       <h1>
         IBT
-        <small>Profiles</small>
+        <small>Krthik</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
