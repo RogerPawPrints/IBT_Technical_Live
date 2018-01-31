f751849f99a72019df78fdf699370240ed60bc10
karthikkct77 <karthikkct77@gmail.com>
2018-01-31 15:20:20 +0530

    attach

--- application/views/User/create_task.php
+++ application/views/User/create_task.php
@@ -181,7 +181,7 @@
 
 
         $("#Project_Select").change(function () {
-             alert("hiiii");
+             //alert("hiiii");
             /*dropdown post *///
             $.ajax({
                 url: "<?php echo site_url('User_Controller/Show_On_Project_Select'); ?>",
