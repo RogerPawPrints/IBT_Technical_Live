11b9bea6a43298bb87bada61dc2f2e7cd506c5c2
karthikkct77 <karthikkct77@gmail.com>
2018-01-31 10:57:28 +0530

    User create task file controller

--- application/controllers/User_Controller.php
+++ application/controllers/User_Controller.php
@@ -31,11 +31,19 @@ class User_Controller extends CI_Controller
     public function Create_Task()
     {
         $this->data['Select_Project']= $this->technical_user_model->Select_Project();
+        //$this->data['Select_Project']= $this->technical_user_model->Show_On_Select_Project();
         $this->load->view('User/header');
         $this->load->view('User/left');
         $this->load->view('User/top');
         $this->load->view('User/create_task',$this->data, FALSE);
         $this->load->view('User/footer');
+    }
+
+    public function Show_On_Project_Select()
+    {
+        $project_code=$this->input->post('id', true);
+        $data= $this->technical_user_model->Show_On_Select_Project( $project_code);
+        echo json_encode($data);
     }
 
 }
