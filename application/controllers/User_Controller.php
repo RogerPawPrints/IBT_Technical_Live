643bcd178c21fd5da59796494777ed56c4916fe2
karthikkct77 <karthikkct77@gmail.com>
2018-01-31 12:55:30 +0530

    User create task file controller

--- application/controllers/User_Controller.php
+++ application/controllers/User_Controller.php
@@ -42,8 +42,17 @@ class User_Controller extends CI_Controller
     public function Show_On_Project_Select()
     {
         $project_code=$this->input->post('id', true);
+
         $data= $this->technical_user_model->Show_On_Select_Project($project_code);
-        echo json_encode($data);
+
+        $resource_name= $this->technical_user_model->Select_Resource($project_code);
+
+        $full_data = array('Client_Details' => $data,
+                            'Resource_Select' => $resource_name
+            );
+
+        echo  json_encode($full_data);
+
     }
 
 }
