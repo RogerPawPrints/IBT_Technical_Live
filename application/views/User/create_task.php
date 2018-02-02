ad1abf6e217aa5ef26d8f4463ec3b30e2285a62e
karthikkct77 <karthikkct77@gmail.com>
2018-02-02 16:30:37 +0530

    file upload

--- application/views/User/create_task.php
+++ application/views/User/create_task.php
@@ -216,3 +216,82 @@
         ele.parentNode.removeChild(ele);
     }
 </script>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $("#startdate").datepicker({
+            todayBtn: 1,
+            autoclose: true,
+            startDate: new Date(),
+        }).on('changeDate', function (selected) {
+            var minDate = new Date(selected.date.valueOf());
+            $('#enddate').datepicker('setStartDate', minDate);
+        });
+
+
+        $("#enddate").datepicker()
+            .on('changeDate', function (selected) {
+                var minDate = new Date(selected.date.valueOf());
+                $('#startdate').datepicker('setEndDate', minDate);
+            });
+
+
+        $("#Project_Select").change(function () {         /*Selecting Project Detais And Resource*/
+            //alert("hiiii");
+            /*dropdown post *///
+            //document.getElementById('Resource_Select').value = '';
+            $.ajax({
+                url: "<?php echo site_url('User_Controller/Show_On_Project_Select'); ?>",
+                data: {
+                    id:
+                        $(this).val()
+                },
+                type: "POST",
+                success: function (data) {
+                    // alert(data);
+                    $("#show_on_project").show();
+                    var task_details = $.parseJSON(data);
+                    //alert(task_details);
+                    var client_name = task_details.Client_Details[0]['Client_Company_Name'];
+                    //alert(client_name);
+                    document.getElementById('Client_Name').value = client_name;
+
+                    var Client_Name_icode = task_details.Client_Details[0]['Client_Icode'];
+                    //alert(client_name);
+                    document.getElementById('Client_Name_icode').value = Client_Name_icode;
+
+                    var work_cat = task_details.Client_Details[0]['WorkCategory_Name'];
+                    document.getElementById('Work_Category').value = work_cat;
+
+                    var work_type = task_details.Client_Details[0]['Work_Name'];
+                    document.getElementById('Work_Type').value = work_type;
+
+                }
+
+
+            });
+        });
+
+
+        $("#Project_Select").change(function () {         /*Selecting Project Detais And Resource*/
+            //alert("hiiii");
+            /*dropdown post *///
+            //document.getElementById('Resource_Select').value = '';
+            $.ajax({
+                url: "<?php echo site_url('User_Controller/Show_On_Project_Resource'); ?>",
+                data: {
+                    id:
+                        $(this).val()
+                },
+                type: "POST",
+                success: function (data) {
+                    $("#Resource_Select").html(data);
+                }
+
+
+            });
+        });
+    });
+
+    </script>
+
